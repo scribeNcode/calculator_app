f3b7be0e6d31a9cc540db17a7aac941d07b08cbd
scribeNcode <deyeye99@gmail.com>
2025-05-08 01:06:04 +0100

    add click eventlistners to displayable button
    all displayable buttons like (0-9 and all arithemetic buttons) will show  on the UI
    display area when clicked while the clear button and equals to button
    will not show on the UI display area

---
+++ script.js
@@ -0,0 +1,35 @@
+// let numberZero = document.querySelector('#numZero_button');
+// let numberOne = document.querySelector('#numOne_button');
+// let numberTwo = document.querySelector('#numTwo_button');
+// let numberThree = document.querySelector('#numThree_button');
+// let numberFour = document.querySelector('#numFour_button');
+// let numberFive = document.querySelector('#numFive_button');
+// let numberSix = document.querySelector('#numSix_button');
+// let numberSeven = document.querySelector('#numSeven_button');
+// let numberEight = document.querySelector('#numEight_button');
+// let numberNine = document.querySelector('#numNine_button');
+
+// console.log(numberOne);
+// console.log(numberTwo);
+// console.log(numberThree);
+// console.log(numberFour);
+// console.log(numberFive);
+// console.log(numberSix);
+// console.log(numberSeven);
+// console.log(numberEight);
+// console.log(numberNine);
+// console.log(numberZero);
+
+// get all buttons
+let allButtons = document.querySelectorAll('.btn-displayable');
+// get the display tag
+let userClickedButtonDisplayArea = document.getElementById('display');
+
+// Method to add event listener to displayable buttons
+allButtons.forEach(button=>{
+    button.addEventListener('click', (e)=>{
+        e.preventDefault();
+        console.log(button.innerText);
+        userClickedButtonDisplayArea.innerText += button.innerText
+    })
+})
