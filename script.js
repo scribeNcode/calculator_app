da4a183a28dc043df3223acdc622d789c60f01ee
scribeNcode <deyeye99@gmail.com>
2025-05-19 00:42:27 +0100

    make app functional

--- script.js
+++ script.js
@@ -20,6 +20,44 @@
 // console.log(numberNine);
 // console.log(numberZero);
 
+// logic to ensure that operational button is not pressed twice :
+let plusBtn = document.getElementById('addition_button').innerText.toString();
+let minusBtn = document.getElementById('minus_button').innerText.toString();
+let divisionBtn = document.getElementById('division_button').innerText.toString();
+let multiplicationBtn = document.getElementById('multiplication_button').innerText.toString();
+
+ 
+function prevDuplicationOfOppBtn(operationBtn){
+    // get the current display value
+    let currentVal = userClickedButtonDisplayArea.innerText
+    
+    // get the last character
+    let lastNumber = currentVal.slice(-1);
+    console.log(lastNumber)
+    if(lastNumber != plusBtn && 
+        lastNumber != minusBtn &&
+        lastNumber != divisionBtn &&
+        lastNumber != multiplicationBtn){
+        userClickedButtonDisplayArea.innerText += operationBtn
+    }
+}
+
+// Add event listener to operation buttons
+let multiplyTag = document.getElementById('multiplication_button');
+let plusTag = document.getElementById('addition_button');
+let minusTag = document.getElementById('minus_button');
+let divisionTag = document.getElementById('division_button')
+
+// add Event Listener
+multiplyTag.addEventListener('click', ()=>prevDuplicationOfOppBtn(multiplicationBtn));
+plusTag.addEventListener('click', ()=>prevDuplicationOfOppBtn(plusBtn));
+minusTag.addEventListener('click', ()=>prevDuplicationOfOppBtn(minusBtn));
+divisionTag.addEventListener('click', ()=>prevDuplicationOfOppBtn(divisionBtn))
+
+
+
+
+
 // get all buttons
 let allButtons = document.querySelectorAll('.btn-displayable');
 // get the display tag
@@ -40,4 +78,31 @@ let clearButton = document.getElementById('clear_button');
 clearButton.addEventListener('click',()=>{
     // clear the userInput displayArea whenever the clear button is cliked
     userClickedButtonDisplayArea.innerText = ''
+});
+
+// method to perform mathematical operation using equals to
+let equalButton = document.getElementById('equals_button');
+// add an event listener to equalButton
+equalButton.addEventListener('click', (e)=>{
+e.preventDefault();
+// check the last character of the user input
+let userInput = userClickedButtonDisplayArea.innerText.slice('');
+let userLastCharacter = userInput.slice(-1);
+let calculatedInput;
+if(userLastCharacter == plusBtn || 
+    userLastCharacter == minusBtn ||
+    userLastCharacter == divisionBtn || 
+    userLastCharacter == multiplicationBtn){
+    let characterToCalculate = userInput.slice(0, -1);
+    console.log(characterToCalculate)
+    calculatedInput = eval(characterToCalculate);
+    console.log(calculatedInput) ;
+    document.getElementById('answer_div').innerText = calculatedInput;
+
+}else{
+    calculatedInput = eval(userInput);
+    console.log(calculatedInput);
+    document.getElementById('answer_div').innerText = calculatedInput;
+}
+
 })
